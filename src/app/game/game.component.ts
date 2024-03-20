caded788ad3423bf8e9af38e1d5cf5e70f6f28bd
DirectDarkside <florianmkube13@gmail.com>
2024-03-20 15:03:07 +0100

    add some comps

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
+export class GameComponent implements OnInit{
+
+  pickCardAnimation = false;
+  currentCard: string | undefined;
+  game: Game | undefined;
+
+  ngOnInit(): void {
+    this.newGame(); 
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    if(!this.pickCardAnimation) {
+      this.currentCard = this.game?.stack.pop();
+      this.pickCardAnimation = true;
+      setTimeout(() => {
+        if(this.currentCard) {
+          this.game?.playedCards.push(this.currentCard);
+        }
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
+  }
 
 }
